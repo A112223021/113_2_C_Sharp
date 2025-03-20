fa15e4e833da377eeaed89cf106702b24f41453d
A112223021 <A112223021@mail.shu.edu.tw>
2025-03-20 12:56:44 +0800

    0320

---
+++ 0320/Tutorial 7-1/Lottery Numbers/Lottery Numbers/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lottery_Numbers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            const int SIZE =5; // The size of the array.
+            int[] lotteryNumbers = new int[SIZE]; // Array to hold the lottery numbers.
+            Random rand = new Random(); // Random object.
+
+            for(int i = 0; i < lotteryNumbers.Length;i++)
+            {
+                lotteryNumbers[i] = rand.Next(1, 43); // Get a random number between 1 and 42.
+            }
+
+            //fifthLabel.Text = lotteryNumbers[0].ToString();
+            //fourthLabel.Text = lotteryNumbers[1].ToString();
+            //thirdLabel.Text = lotteryNumbers[2].ToString();
+            //secondLabel.Text = lotteryNumbers[3].ToString();
+            //firstLabel.Text = lotteryNumbers[4].ToString();
+
+            Label[] showlabels = { fifthLabel, fourthLabel, thirdLabel, secondLabel, firstLabel };
+            for (int i = 0; i < lotteryNumbers.Length; i++)
+            {
+                showlabels[i].Text = lotteryNumbers[i].ToString();
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+    }
+}
