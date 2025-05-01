533a6a1ab5e141a0bff67d715e9686030e140fd2
A112223021 <A112223021@mail.shu.edu.tw>
2025-05-01 13:04:59 +0800

    0501

---
+++ 0501/Password Validation/Password Validation/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Password_Validation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int NumberUpperCase(string str)
+        {
+            int count = 0;
+            foreach (char c in str)
+            {
+                if (char.IsUpper(c))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        private int NumberLowerCase(string str)
+        {
+            int count = 0;
+            foreach (char c in str)
+            {
+                if (char.IsLower(c))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        private int NumberDigits(string str)
+        {
+            int count = 0;
+            foreach (char c in str)
+            {
+                if (char.IsDigit(c))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        private void checkPasswordButton_Click(object sender, EventArgs e)
+        {
+            const int MIN_LENGTH = 8;
+            string password = passwordTextBox.Text;
+            if (password.Length >= MIN_LENGTH &&
+                NumberUpperCase(password) > 0 &&
+                NumberLowerCase(password) > 0 &&
+                NumberDigits(password) > 0)
+            {
+                MessageBox.Show("Password is valid.", "Validation Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Password is invalid. It must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, and one digit.", "Validation Result", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void passwordTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
